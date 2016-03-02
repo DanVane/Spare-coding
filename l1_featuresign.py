ee64001ae360512981a236b2085f5e5f5dd868f0
stevenydc <stevenydc@hotmail.com>
2016-03-02 11:40:32 -0800

    finished line search, still different result from matlab code

--- l1_featuresign.py
+++ l1_featuresign.py
@@ -20,7 +20,7 @@ def l1ls_featuresign(A, Y, gamma, Xinit=np.array([])):
         AtY = np.dot(A.transpose(), Y).ravel()
     except ValueError, e:
         print "ERROR: Dimension of A or Dimension of Y not valid"
-        print "Y can be 1D numpy array, or 2D matrix with number or rows == number of rows of A"
+        print "Y can be 1D numpy array, or 2D matrix with number or rows¡ == number of rows of A"
         print e
 
     #rankA is not actually the rank of A, just a limit on dimension
@@ -62,9 +62,9 @@ testing first function
 A = np.array([[1,2,3],[4,5,6],[7,8,9]])
 y = np.array([1,5,5])
 gamma = 0.2
-X = np.array([[2,1,2]])
+X = np.array([2,1,2])
 
-ret = l1ls_featuresign(A, y, gamma)
+ret = l1ls_featuresign(A, y, gamma, X)
 
 
 
@@ -99,6 +99,8 @@ def ls_featuresign_sub(A, y, AtA, Aty, gamma, xinit = np.array([])):
     for i in range(ITERMAX):
         act_idx0 = np.where(act == 0)[0] #TODO: this does not work well when act is sparse
         print "act_idx0", act_idx0
+
+        # Step 2:
         #TODO: Is this the Gradient of ||y-Ax||^2 wrt every x?
         grad = np.dot(AtA, x) - Aty
         print "grad =",grad
@@ -137,7 +139,7 @@ def ls_featuresign_sub(A, y, AtA, Aty, gamma, xinit = np.array([])):
         while True:
             k += 1
             if k>ITERMAX:
-                print "Exceeding maximum number of iterations, solution might not be optimal"
+                print "(b)Exceeding maximum number of iterations, solution might not be optimal"
                 return x, fobj
             if len(act_idx1) == 0:
                 if allowZero:
@@ -156,9 +158,9 @@ def ls_featuresign_sub(A, y, AtA, Aty, gamma, xinit = np.array([])):
                 continue #TODO: continue just goes to the next while iteration... does this even do anything?
 
     if i >= ITERMAX:
-        print "Exceeding maximum number of iterations, solution might not be optimal"
+        print "(a)Exceeding maximum number of iterations, solution might not be optimal"
 
-    fobj = fobj_featuresign(x, A, y, AtA, Aty, gamma)
+    fobj, objA = fobj_featuresign(x, A, y, AtA, Aty, gamma)
     return x, fobj
 
 
@@ -179,12 +181,71 @@ def compute_FS_step(x, A, y, AtA, Aty, theta, act, act_idx1, gamma):
     x_new = np.linalg.solve(AtA2, Aty[act_idx1] - gamma*theta2)
 
     optimality1 = False
-
-    print "we did it"
-    lsearch = 1
+    # lsearch = 1
+    # TODO: consider changing to fobj, objA = fobj_featuresign(...)
     fobj = 0
 
-    '''THE PART DIEN IS WORKING ON... basically line search '''
+
+    # TODO: why does equal sign imply condition A is satisfied?
+    # This step seems to be checking optimality1.. which seems to be condition a in the paper
+    # But the conditions used seem to be quite different
+
+    if all(np.sign(x_new) == np.sign(x2)):
+        # I think optimality1 == condition A and is true when
+        # objA == 0, or < eps
+        optimality1 = True
+
+        x[act_idx1] = x_new
+        lsearch = 1
+        # The following comment from author seems to be what the original condition should be
+        # But for some reason we assumed this to be 0
+        fobj = 0 # fob_featuresign(x, A, y, AtA, Aty, gamma)
+        print "Optimality 1 is satisfied in compute_FS_step!"
+        return [x, theta, act, act_idx1, optimality1, lsearch, fobj]
+    
+
+
+    ''' line search '''
+    progress = np.true_divide((0-x2), x_new-x2)
+    lsearch = 0
+    a= 0.5*sum(np.power(np.dot(A[:, act_idx1],(x_new- x2)), 2))
+    b= np.dot(x2,np.dot(AtA2,(x_new- x2))) - np.dot(np.transpose(x_new- x2),Aty[act_idx1])
+
+    fobj_lsearch = gamma * sum(abs(x2))
+
+    # TODO: chekc if progress is row vector!
+    prog_with_idx = zip(np.hstack([progress,1]), range(len(progress)+1))
+
+    [sort_lsearch, ix_lsearch] = zip(*sorted(prog_with_idx, key=lambda x: x[0]))
+
+    remove_idx=[]
+    for i in range(len(sort_lsearch)):
+        t = sort_lsearch[i]
+        if t<=0 or t>1:
+            continue
+        s_temp= x2+ (x_new- x2)*t
+        fobj_temp = a*t**2 + b*t + gamma*sum(abs(s_temp)) # ======> pay attention here... what's going on?
+            #What is fobj_temp? what is fobj?
+            #I think it is the objective function
+        if fobj_temp < fobj_lsearch:
+            fobj_lsearch = fobj_temp
+            lsearch = t
+            if t<1:
+                remove_idx.append(ix_lsearch[i]) # remove_idx can be more than two..
+        elif fobj_temp > fobj_lsearch:
+            break
+        else:
+            if (sum(x2==0)) == 0:
+                lsearch = t
+                fobj_lsearch = fobj_temp;
+                # remove_idx can be more than two..
+                if t<1:
+                    remove_idx.append(ix_lsearch[i])
+
+
+
+
+
 
 
     # Updating the result of line search I THINK
@@ -200,8 +261,8 @@ def compute_FS_step(x, A, y, AtA, Aty, theta, act, act_idx1, gamma):
 
         theta[act_idx1[remove_idx]] = 0
         act[act_idx1[remove_idx]] = 0
-        act_idx1 = act_idx1[remove_idx]re
-
+        # act_idx1 = act_idx1[remove_idx]
+        act_idx1 = np.where(act != 0)[0]
 
     return [x, theta, act, act_idx1, optimality1, lsearch, fobj]
 
